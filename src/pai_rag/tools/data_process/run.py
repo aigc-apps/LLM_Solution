613d89fc0506b41cf2ed32c0207377723d1ff20c
筱文 <zxw320697@alibaba-inc.com>
2025-01-03 10:21:15 +0800

    Remove config file

--- src/pai_rag/tools/data_process/run.py
+++ src/pai_rag/tools/data_process/run.py
@@ -1,52 +1,100 @@
-from loguru import logger
-from pai_rag.tools.data_process.ray_executor import RayExecutor
 import argparse
-from pai_rag.tools.data_process.ops.base_op import OPERATORS
-import yaml
+from loguru import logger
+from typing import List
+from pai_rag.tools.data_process.ray_executor import RayExecutor
 
 
-def extract_parameters(yaml_dict, cfg):
-    print("yaml_dict", yaml_dict)
-    extracted_params = {key: value for key, value in yaml_dict.items() if key != "op"}
-    extracted_params["working_dir"] = cfg.working_dir
-    extracted_params["dataset_path"] = cfg.dataset_path
-    extracted_params["export_path"] = cfg.export_path
-    return extracted_params
+def process_parser(
+    dataset_path,
+    export_path,
+    working_dir,
+    cpu_required,
+    mem_required,
+    accelerator,
+    enable_mandatory_ocr,
+    concat_csv_rows,
+    enable_table_summary,
+    format_sheet_data_to_json,
+    sheet_column_filters,
+):
+    parser_dict = {}
+    parser_dict["dataset_path"] = dataset_path
+    parser_dict["export_path"] = export_path
+    parser_dict["working_dir"] = working_dir
+    parser_dict["cpu_required"] = cpu_required
+    parser_dict["mem_required"] = mem_required
+    parser_dict["accelerator"] = accelerator
+    parser_dict["enable_mandatory_ocr"] = enable_mandatory_ocr
+    parser_dict["concat_csv_rows"] = concat_csv_rows
+    parser_dict["enable_table_summary"] = enable_table_summary
+    parser_dict["format_sheet_data_to_json"] = format_sheet_data_to_json
+    parser_dict["sheet_column_filters"] = sheet_column_filters
+    return parser_dict
 
 
-def update_op_process(cfg):
-    op_keys = list(OPERATORS.modules.keys())
-    logger.info(f"Loading all operation keys: {op_keys}")
+def process_splitter(
+    dataset_path,
+    export_path,
+    working_dir,
+    cpu_required,
+    mem_required,
+    type,
+    chunk_size,
+    chunk_overlap,
+    enable_multimodal,
+):
+    splitter_dict = {}
+    splitter_dict["dataset_path"] = dataset_path
+    splitter_dict["export_path"] = export_path
+    splitter_dict["working_dir"] = working_dir
+    splitter_dict["cpu_required"] = cpu_required
+    splitter_dict["mem_required"] = mem_required
+    splitter_dict["type"] = type
+    splitter_dict["chunk_size"] = chunk_size
+    splitter_dict["chunk_overlap"] = chunk_overlap
+    splitter_dict["enable_multimodal"] = enable_multimodal
+    return splitter_dict
 
-    if cfg.process is None:
-        cfg.process = []
 
-    with open(cfg.config) as file:
-        process_cfg = yaml.safe_load(file)
-    for i, process_op in enumerate(process_cfg["process"]):
-        if process_op["op"] in op_keys:
-            cfg.process.append(process_op["op"])
-            cfg.process[i] = {process_op["op"]: extract_parameters(process_op, cfg)}
-
-    return cfg
+def process_embedder(
+    dataset_path,
+    export_path,
+    working_dir,
+    cpu_required,
+    mem_required,
+    accelerator,
+    source,
+    model,
+    enable_sparse,
+    multimodal_source,
+    enable_multimodal,
+):
+    embedder_dict = {}
+    embedder_dict["dataset_path"] = dataset_path
+    embedder_dict["export_path"] = export_path
+    embedder_dict["working_dir"] = working_dir
+    embedder_dict["cpu_required"] = cpu_required
+    embedder_dict["mem_required"] = mem_required
+    embedder_dict["accelerator"] = accelerator
+    embedder_dict["source"] = source
+    embedder_dict["model"] = model
+    embedder_dict["enable_sparse"] = enable_sparse
+    embedder_dict["multimodal_source"] = multimodal_source
+    embedder_dict["enable_multimodal"] = enable_multimodal
+    return embedder_dict
 
 
 def init_configs():
     """
-    initialize the jsonargparse parser and parse configs from one of:
+    initialize the argparse parser and parse configs from one of:
         1. POSIX-style commands line args;
-        2. config files in yaml (json and jsonnet supersets);
-        3. environment variables
-        4. hard-coded defaults
+        2. environment variables
+        3. hard-coded defaults
 
     :return: a global cfg object used by the Executor or Analyzer
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--config",
-        help="Path to a dj basic configuration file.",
-        required=True,
-    )
+    # shared parameters
     parser.add_argument(
         "--dataset_path",
         type=str,
@@ -69,16 +117,182 @@ def init_configs():
         default="/PAI-RAG",
         help="Path to working dir for ray cluster.",
     )
-    parser.add_argument("--process", type=int, default=None, help="name of processes")
-    cfg = parser.parse_args()
-    cfg = update_op_process(cfg)
-    return cfg
+    parser.add_argument(
+        "--cpu_required",
+        type=int,
+        default=1,
+        help="Cpu required for each rag operator.",
+    )
+    parser.add_argument(
+        "--mem_required",
+        type=str,
+        default="1GB",
+        help="Memory required for each rag operator.",
+    )
+
+    # Add subparsers
+    subparsers = parser.add_subparsers(
+        dest="operator", help="Choose a rag operator to run"
+    )
+
+    # SubParser for rag_parser
+    parser_parser = subparsers.add_parser("rag_parser", help="Run rag_parser")
+    parser_parser.add_argument(
+        "--accelerator",
+        type=str,
+        default="cpu",
+        help="Accelerator type for pai_rag_parser and pai_rag_embedder operator.",
+    )
+    parser_parser.add_argument(
+        "--enable_mandatory_ocr",
+        type=bool,
+        default=False,
+        help="Whether to enable mandatory OCR for pai_rag_parser operator.",
+    )
+    parser_parser.add_argument(
+        "--concat_csv_rows",
+        type=bool,
+        default=False,
+        help="Whether to concat csv rows for pai_rag_parser operator.",
+    )
+    parser_parser.add_argument(
+        "--enable_table_summary",
+        type=bool,
+        default=False,
+        help="Whether to enable table summary for pai_rag_parser operator.",
+    )
+    parser_parser.add_argument(
+        "--format_sheet_data_to_json",
+        type=bool,
+        default=False,
+        help="Whether to format sheet data to json for pai_rag_parser operator.",
+    )
+    parser_parser.add_argument(
+        "--sheet_column_filters",
+        type=List[str],
+        default=[],
+        help="Column filters for pai_rag_parser operator.",
+    )
+
+    # SubParser for rag_splitter
+    parser_splitter = subparsers.add_parser("rag_splitter", help="Run rag_splitter")
+    parser_splitter.add_argument(
+        "--type",
+        type=str,
+        default="Token",
+        help="Split type for pai_rag_splitter operator.",
+    )
+    parser_splitter.add_argument(
+        "--chunk_size",
+        type=int,
+        default=1024,
+        help="Chunk size for pai_rag_splitter operator.",
+    )
+    parser_splitter.add_argument(
+        "--chunk_overlap",
+        type=int,
+        default=20,
+        help="Chunk overlap for pai_rag_splitter operator.",
+    )
+    parser_splitter.add_argument(
+        "--enable_multimodal",
+        type=bool,
+        default=False,
+        help="Whether to enable multimodal for pai_rag_splitter and pai_rag_embedder operator.",
+    )
+
+    # SubParser for rag_embedder
+    parser_embedder = subparsers.add_parser("rag_embedder", help="Run rag_embedder")
+    parser_embedder.add_argument(
+        "--accelerator",
+        type=str,
+        default="cpu",
+        help="Accelerator type for pai_rag_parser and pai_rag_embedder operator.",
+    )
+    parser_embedder.add_argument(
+        "--source",
+        type=str,
+        default="huggingface",
+        help="Embedding model source for pai_rag_embedder operator.",
+    )
+    parser_embedder.add_argument(
+        "--model",
+        type=str,
+        default="bge-large-zh-v1.5",
+        help="Embedding model name for pai_rag_embedder operator.",
+    )
+    parser_embedder.add_argument(
+        "--enable_sparse",
+        type=bool,
+        default=False,
+        help="Whether to enable sparse for pai_rag_embedder operator.",
+    )
+    parser_embedder.add_argument(
+        "--multimodal_source",
+        type=str,
+        default="cnclip",
+        help="Multi-modal embedding model source for pai_rag_embedder operator.",
+    )
+    parser_embedder.add_argument(
+        "--enable_multimodal",
+        type=bool,
+        default=False,
+        help="Whether to enable multimodal for pai_rag_splitter and pai_rag_embedder operator.",
+    )
+
+    args = parser.parse_args()
+
+    # Determine which process to run with shared and unique parameters
+    if args.operator == "rag_parser":
+        op_params = process_parser(
+            args.dataset_path,
+            args.export_path,
+            args.working_dir,
+            args.cpu_required,
+            args.mem_required,
+            args.accelerator,
+            args.enable_mandatory_ocr,
+            args.concat_csv_rows,
+            args.enable_table_summary,
+            args.format_sheet_data_to_json,
+            args.sheet_column_filters,
+        )
+    elif args.operator == "rag_splitter":
+        op_params = process_splitter(
+            args.dataset_path,
+            args.export_path,
+            args.working_dir,
+            args.cpu_required,
+            args.mem_required,
+            args.type,
+            args.chunk_size,
+            args.chunk_overlap,
+            args.enable_multimodal,
+        )
+    elif args.operator == "rag_embedder":
+        op_params = process_embedder(
+            args.dataset_path,
+            args.export_path,
+            args.working_dir,
+            args.cpu_required,
+            args.mem_required,
+            args.accelerator,
+            args.source,
+            args.model,
+            args.enable_sparse,
+            args.multimodal_source,
+            args.enable_multimodal,
+        )
+    else:
+        parser.print_help()
+
+    return args, op_params
 
 
 @logger.catch(reraise=True)
 def main():
-    cfg = init_configs()
-    executor = RayExecutor(cfg)
+    cfg, op_params = init_configs()
+    executor = RayExecutor(cfg, op_params)
     executor.run()
 
 
