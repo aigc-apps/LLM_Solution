fac878cffdf97b119806af6e4b68eefb31a8e7c7
筱文 <zxw320697@alibaba-inc.com>
2025-01-20 20:41:09 +0800

    Fix reviews

--- src/pai_rag/tools/data_process/ops/parser_op.py
+++ src/pai_rag/tools/data_process/ops/parser_op.py
@@ -72,7 +72,7 @@ class Parser(BaseOP):
         else:
             self.should_replace = False
             logger.warning(
-                "Environment variables for paths are not fully set. Path replacement will be skipped."
+                "File path won't be replaced to data source URI since either INPUT_MOUNT_PATH or OSS_SOURCE_PATH is not provided."
             )
         logger.info(
             f"""ParserActor [PaiDataReader] init finished with following parameters:
@@ -87,30 +87,25 @@ class Parser(BaseOP):
         )
 
     def replace_mount_with_real_path(self, document):
-        if not self.should_replace:
-            return document
-
-        try:
+        if self.should_replace:
             file_path = document.metadata["file_path"]
             file_path_obj = Path(file_path).resolve()
-            relative_path_str = (
-                file_path_obj.relative_to(self.mount_path).as_posix().strip("/")
-            )
-            document.metadata["file_path"] = f"{self.real_path}/{relative_path_str}"
-            document.metadata["mount_path"] = file_path
-            logger.debug(
-                f"Replacing path: {file_path} --> {document.metadata['file_path']}"
-            )
-            return document
-        except ValueError:
-            # file_path 不以 mount_path 开头
-            logger.debug(
-                f"Path {document.metadata['file_path']} does not start with mount path {self.mount_path}. No replacement done."
-            )
-            return document
-        except Exception as e:
-            logger.error(f"Error replacing path {document.metadata['file_path']}: {e}")
-            return document
+            try:
+                relative_path_str = (
+                    file_path_obj.relative_to(self.mount_path).as_posix().strip("/")
+                )
+                document.metadata["file_path"] = f"{self.real_path}/{relative_path_str}"
+                document.metadata["mount_path"] = file_path
+                logger.debug(
+                    f"Replacing original file_path: {file_path} --> {document.metadata['file_path']}"
+                )
+            except ValueError:
+                # file_path 不以 mount_path 开头
+                logger.debug(
+                    f"Path {file_path} does not start with mount path {self.mount_path}. No replacement done."
+                )
+            except Exception as e:
+                logger.error(f"Error replacing path {file_path}: {e}")
 
     def process(self, input_file):
         current_thread = threading.current_thread()
@@ -119,4 +114,5 @@ class Parser(BaseOP):
         if len(documents) == 0:
             logger.info(f"No data found in the input file: {input_file}")
             return None
-        return convert_document_to_dict(self.replace_mount_with_real_path(documents[0]))
+        self.replace_mount_with_real_path(documents[0])
+        return convert_document_to_dict(documents[0])
